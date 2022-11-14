3a84847c062284359f9e19138eb6858640974fcc
Game Development Club WVSS <austinjiangboyu@gmail.com>
2022-11-13 18:11:24 -0800

    First Commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
 /*    Author:       C:\Users\austi                                            */
-/*    Created:      Mon Nov 07 2022                                           */
+/*    Created:      Sun Nov 13 2022                                           */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
